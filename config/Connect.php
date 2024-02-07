020fcda2d19ad09abc841cf5fedd61601b7374a1
Felipe <lipestaub@gmail.com>
2024-02-07 16:50:08 -0300

    change dbname

--- config/Connect.php
+++ config/Connect.php
@@ -4,7 +4,7 @@
         public static function getConnection()
         {
             try {
-                return new PDO('pgsql:host=localhost;dbname=trivia', 'postgres', 'postgres');
+                return new PDO('pgsql:host=localhost;dbname=project_manager', 'postgres', 'postgres');
             }
             catch (PDOException $exception) {
                 echo "Connection failed: " . $exception->getMessage();
