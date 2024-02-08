94572be44d6e1b7c39f3a4329796ef3b4d7d7bd1
Rafazanella01 <rafazanella2004@gmail.com>
2024-02-08 14:11:14 -0300

    add css

--- views/userTasks.php
+++ views/userTasks.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $user->getName(); ?>'s Tasks</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+
 </head>
 <body>
     <?php include_once __DIR__ . '/../public/html/menu.html'; ?>
